14162def7467bddebb50e748a6606304d63c167c
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2016-06-20 16:31:07 +0200

    tpd.New: fix incorrect selection of constructor symbol for complex types

--- src/dotty/tools/dotc/ast/tpd.scala
+++ src/dotty/tools/dotc/ast/tpd.scala
@@ -383,7 +383,7 @@ object tpd extends Trees.Instance[Type] with TypedTreeInfo {
 
   /** new C(args), calling the primary constructor of C */
   def New(tp: Type, args: List[Tree])(implicit ctx: Context): Apply =
-    New(tp, tp.typeSymbol.primaryConstructor.asTerm, args)
+    New(tp, tp.widenDealias.typeSymbol.primaryConstructor.asTerm, args)
 
   /** new C(args), calling given constructor `constr` of C */
   def New(tp: Type, constr: TermSymbol, args: List[Tree])(implicit ctx: Context): Apply = {
